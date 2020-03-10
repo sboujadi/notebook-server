40fff3579512cb7afbde03565053e15005063152
BOUJADI SOUKAINA <boujadi.soukaina@gmail.com>
2020-03-10 22:58:51 +0100

    init project

---
+++ src/main/java/com/notebook/notebookserver/NotebookServerApplication.java
@@ -0,0 +1,13 @@
+package com.notebook.notebookserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotebookServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotebookServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/notebook/notebookserver/NotebookServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.notebook.notebookserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotebookServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
